032e89b31eaf6d29dc1553b33c28c57854ba4062
JojoDuke <jojoamankwa@gmail.com>
2022-12-11 11:54:02 +0000

    navbar

--- base/views.py
+++ base/views.py
@@ -2,8 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+rooms = [
+    {'id': 1, 'name': 'Learn Python'},
+    {'id': 2, 'name': 'Its Django'},
+    {'id': 3, 'name': 'Go Flask'}
+]
+
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {'rooms': rooms})
 
 def rooms(request):
     return HttpResponse("Welcome to rooms")
